6b942575009567bcb01c37f1bdccffeee42a09b2
vincent ndisi <vndis1298@yahoo.com>
2018-08-21 21:14:52 +0000

    Done.

---
+++ learn.js
@@ -0,0 +1,18 @@
+// declare the variable 
+var word 
+
+// assign a value to the variable 
+word = 'bird'
+
+console.log(word) // 'bird' 
+
+// assign another value to the variable 
+word = 'dog'
+
+console.log(word) // 'dog'
+
+var mynote = "get milk" 
+
+// later on 
+
+mynote = "get cookies"
